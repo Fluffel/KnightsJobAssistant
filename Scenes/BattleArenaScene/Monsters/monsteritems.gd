b2bc93ef54bea3624397a78c0037aee524afc26f
Fluffel <flokandra9@gmail.com>
2016-07-17 23:01:12 +0200

    items werden je nach landahoehe kleiner oder groesser, ihr einsammeln braucht noch verbesserungsbedarf

--- Scenes/BattleArenaScene/Monsters/monsteritems.gd
+++ Scenes/BattleArenaScene/Monsters/monsteritems.gd
@@ -2,7 +2,7 @@
 extends Node2D
 
 var ges = Vector2(-randi() %80 -20, -randi() %100 -20)
-var gravity = 100
+var gravity = 75
 var bes = Vector2(0, gravity)
 
 var r = randf()
@@ -14,8 +14,8 @@ var posAfterLanding = Vector2()
 var spawnPos = Vector2()
 
 var sizeAfterLanding
-var sizemax = 2
-var sizemin = 0.5
+var sizemax = 1.25
+var sizemin = 0.75
 var sizeChange = Vector2()
 
 var timer = Timer.new()
@@ -30,10 +30,11 @@ func _ready():
 	
 
 func _process(delta):
-	#set_scale(get_scale() + sizeChange * (delta / flyingTime))                                      #(factorLandingpos - spawnPos) * (delta / flyingTime)
+	set_scale(get_scale() + sizeChange * (delta / flyingTime))                                      #(factorLandingpos - spawnPos) * (delta / flyingTime)
 	
 	set_pos(get_pos() + ges * delta)
 	if ges.y  > 0 and get_pos().y >= factorLandingpos:
+		print("ges.x war: ", ges.x * delta)
 		get_parent().get_node("Assistant").appendInformation(get_pos(), get_scale())
 		print("realTimer: ", timer.get_time_left())
 		print("realLPos: ", get_pos())
@@ -44,12 +45,25 @@ func _process(delta):
 func calculateMovement():
 	var posAfterMoveUp
 	var distanceDown
+	print ("ges.: ", ges.y)
+	print ("bes.: ", bes.y)
+	print ("Landingos: ", factorLandingpos)
+	print ("spawnPos: ", spawnPos)
 	
 	flyingTimeUp = abs(ges.y) / bes.y
+	print ("flyingTimeUp: ", flyingTimeUp)
 	posAfterMoveUp = spawnPos.y + (0.5 * flyingTimeUp * ges.y)
-	distanceDown = factorLandingpos - posAfterMoveUp
-	flyingTime = sqrt(distanceDown / bes.y) + ges.y / bes.y
-	posAfterLanding = Vector2(spawnPos.x + flyingTime * ges.x, factorLandingpos)
+	print ("posAfterMoveUp: ", posAfterMoveUp)
+	if posAfterMoveUp >= factorLandingpos:
+		distanceDown = 0
+		print ("distanceDown: ", distanceDown)
+		flyingTime = flyingTimeUp
+		posAfterLanding = Vector2(spawnPos.x + flyingTime * ges.x, posAfterMoveUp)
+	else:
+		distanceDown = factorLandingpos - posAfterMoveUp
+		print ("distanceDown: ", distanceDown)
+		flyingTime = sqrt(distanceDown / (0.5 * bes.y)) + flyingTimeUp
+		posAfterLanding = Vector2(spawnPos.x + flyingTime * ges.x, factorLandingpos)
 	
 	print("calculatedPosAL: ", posAfterLanding)
 	print("expectedFlyingtime: ", flyingTime)
@@ -57,9 +71,9 @@ func calculateMovement():
 	
 func scaleCalculation():
 	var maxDiffLandingpos = 300 / spawnPos.y
-	var minDiffLandingpos = 100 / spawnPos.y
+	var minDiffLandingpos = 100 / spawnPos.y # max und min in factorLandinpos definiert
 	
-	sizeAfterLanding = ((1.5 / (sizemax - sizemin)) * (factorLandingpos / spawnPos.y)) + 0.5
+	sizeAfterLanding = (sizemax - sizemin) * ((factorLandingpos / spawnPos.y) - 100 / spawnPos.y) + sizemin #lineare funktion: (steigung) * ((verhaeltnis der landepos zur sartpos) - kleinstes verhaeltnis) + niedrigster wert
 	sizeChange = sizeAfterLanding * get_scale() - get_scale()
 	#print("sizeShouldBe: ", sizeAfterLanding)
 	#print("sizeWillChange: ", sizeChange)
